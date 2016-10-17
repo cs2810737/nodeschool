d7812451529b05a1493a30fa542f4188aa15a4fe
cs2810737 <cs2810737@gmail.com>
2016-10-17 11:45:23 +0300

    Initial commit

---
+++ scopes-chains-closures/closures.js
@@ -0,0 +1,10 @@
+//doesn't test
+
+function foo (){
+	quux = 343
+	var bar
+	return function zip(){
+		bar = true
+		var quux = 434
+	}
+}
---
+++ scopes-chains-closures/global_scope&shadowing.js
@@ -0,0 +1,7 @@
+function foo (){
+	quux = 343
+	var bar
+	function zip(){
+		var quux = 434
+	}
+}
